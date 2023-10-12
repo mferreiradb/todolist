2dcf2102c88b20f209c76e50e63f6e3ad2dacf61
mferreiradb <mauhmg@gmail.com>
2023-10-12 00:19:30 -0300

    Added tasks module

---
+++ src/main/java/br/com/mferreiradb/todolist/task/TaskController.java
@@ -0,0 +1,23 @@
+package br.com.mferreiradb.todolist.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private ITaskRepository _taskRepository;
+
+    @PostMapping
+    public ResponseEntity<TaskModel> create(@RequestBody TaskModel body) {
+        var createdTask = this._taskRepository.save(body);
+
+        return ResponseEntity.status(201).body(createdTask);
+    }
+}
---
+++ src/main/java/br/com/mferreiradb/todolist/task/TaskModel.java
@@ -0,0 +1,37 @@
+package br.com.mferreiradb.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tbl_tasks")
+public class TaskModel {
+    
+    @Id
+    @GeneratedValue(generator = "UUID")
+
+    private UUID id;
+
+    private UUID userId;
+
+    private String description;
+    
+    @Column(length = 50)
+    private String title;
+    
+    private LocalDateTime startedAt;
+    private LocalDateTime endAt;
+    private String priority;
+    
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
