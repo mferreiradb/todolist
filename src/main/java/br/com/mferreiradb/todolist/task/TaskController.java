469e1b00adc27ff0d5fa86b3ac7610a35e3c2645
mferreiradb <mauhmg@gmail.com>
2023-10-13 00:06:43 -0300

    Removed sout

--- src/main/java/br/com/mferreiradb/todolist/task/TaskController.java
+++ src/main/java/br/com/mferreiradb/todolist/task/TaskController.java
@@ -16,7 +16,6 @@ public class TaskController {
 
     @PostMapping
     public ResponseEntity<TaskModel> create(@RequestBody TaskModel body) {
-        System.out.println("Controller");
         var createdTask = this._taskRepository.save(body);
 
         return ResponseEntity.status(201).body(createdTask);
