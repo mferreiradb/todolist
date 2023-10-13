9fd9840ae052382d0b9a65676c6881867ebdecfe
mferreiradb <mauhmg@gmail.com>
2023-10-13 15:20:55 -0300

    Added date validatioin

--- src/main/java/br/com/mferreiradb/todolist/task/TaskController.java
+++ src/main/java/br/com/mferreiradb/todolist/task/TaskController.java
@@ -1,8 +1,10 @@
 package br.com.mferreiradb.todolist.task;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,11 +21,22 @@ public class TaskController {
     private ITaskRepository _taskRepository;
 
     @PostMapping
-    public ResponseEntity<TaskModel> create(@RequestBody TaskModel body, HttpServletRequest request) {
+    public ResponseEntity create(@RequestBody TaskModel body, HttpServletRequest request) {
         var userId = request.getAttribute("userId");
         body.setUserId((UUID) userId);
+        
+        var currentDate = LocalDateTime.now();
+
+        if(currentDate.isAfter(body.getStartedAt()) || currentDate.isAfter(body.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("As datas de inicio e fim devem ser maiores ou iguais à data atual.");
+        }
+        if(body.getStartedAt().isAfter(body.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de inicio não pode ser superior à data de fim");
+        }
+        
+
         var createdTask = this._taskRepository.save(body);
 
-        return ResponseEntity.status(201).body(createdTask);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdTask);
     }
 }
